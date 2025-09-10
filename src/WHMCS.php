ff18f8c8abcd509179c075945156a6ea32c47ea9
aleksandar.naydenov <aleksandar.naydenov@delta.bg>
2025-09-10 18:30:48 +0300

    Refactored WHMCS initialization in `whmcs-shell` and added `WHMCS` helper class

---
+++ src/WHMCS.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aon4o\WhmcsHelpers;
+
+class WHMCS
+{
+    /**
+     * @param  string  $function
+     * @param  mixed|null  $request
+     * @param  mixed|null  $response
+     *
+     * @return void
+     */
+    public static function log(string $function, mixed $request = null, mixed $response = null): void
+    {
+        $module = basename(dirname(__FILE__, 5));
+
+        logModuleCall($module, $function, $request, $response);
+    }
+
+    /**
+     * Used to initialize the WHMCS system when running scripts from CLI.
+     * 'init.php' is at the root of your WHMCS installation.
+     *
+     * @return void
+     */
+    public static function init(): void
+    {
+        require_once __DIR__ . '/../../../../../../../init.php';
+    }
+}
