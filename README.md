1e61f46262b74670493598c2f5db93c27595074a
aleksandar.naydenov <aleksandar.naydenov@delta.bg>
2025-09-12 12:42:27 +0300

    Standardized parameter naming in LocalAPI classes to use snake_case format for consistency across WHMCS integrations. Marked `Servers.php` as completed in README.

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ This is where your description should go. Try and limit it to a paragraph or two
 - [x] [Orders.php](src/LocalAPI/Orders.php)
 - [x] [Products.php](src/LocalAPI/Products.php)
 - [x] [ProjectManagement.php](src/LocalAPI/ProjectManagement.php)
-- [ ] [Servers.php](src/LocalAPI/Servers.php)
+- [x] [Servers.php](src/LocalAPI/Servers.php)
 - [ ] [Service.php](src/LocalAPI/Service.php)
 - [ ] [Support.php](src/LocalAPI/Support.php)
 - [ ] [System.php](src/LocalAPI/System.php)
--- src/LocalAPI/Servers.php
+++ src/LocalAPI/Servers.php
@@ -9,9 +9,9 @@ class Servers extends LocalAPI
     /**
      * @link https://developers.whmcs.com/api-reference/{$slug}/
      */
-    public function getHealthStatus(null|bool $fetchStatus = null): array
+    public function getHealthStatus(null|bool $fetch_status = null): array
     {
-        return self::call('GetHealthStatus', self::payload(['fetchStatus' => $fetchStatus]));
+        return self::call('GetHealthStatus', self::payload(['fetchStatus' => $fetch_status]));
     }
 
     /**
@@ -20,9 +20,12 @@ class Servers extends LocalAPI
     public function getServers(
         null|int $service_id = null,
         null|int $addon_id = null,
-        null|bool $fetchStatus = null,
+        null|bool $fetch_status = null,
     ): array {
-        return self::call('GetServers',
-            self::payload(['serviceId' => $service_id, 'addonId' => $addon_id, 'fetchStatus' => $fetchStatus]));
+        return self::call('GetServers', self::payload([
+            'serviceId' => $service_id,
+            'addonId' => $addon_id,
+            'fetchStatus' => $fetch_status,
+        ]));
     }
 }
