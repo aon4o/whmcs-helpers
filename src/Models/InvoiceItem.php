1603df2e6f7a257f6ba9bd46d7d3ff757cd1ff98
aleksandar.naydenov <aleksandar.naydenov@delta.bg>
2026-01-05 16:56:02 +0200

    Add itemable relationship method to InvoiceItem model for dynamic relation handling

--- src/Models/InvoiceItem.php
+++ src/Models/InvoiceItem.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Aon4o\WhmcsHelpers\Models;
 
+use Aon4o\WhmcsHelpers\Enums\InvoiceItemType;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use RuntimeException;
 
 /**
  * @property int $id
@@ -38,5 +40,35 @@ class InvoiceItem extends Model
     public function invoice(): BelongsTo
     {
         return $this->belongsTo(Invoice::class, 'invoiceid');
+    }
+
+    /**
+     * @return Model
+     */
+    public function itemable(): Model
+    {
+        $rel_class = match ($this->type) {
+            InvoiceItemType::Hosting => Service::class,
+            InvoiceItemType::Invoice => Invoice::class,
+            InvoiceItemType::Domain,
+            InvoiceItemType::Upgrade,
+            InvoiceItemType::PromoHosting,
+            InvoiceItemType::DomainTransfer,
+            InvoiceItemType::DomainRegister,
+            InvoiceItemType::Addon,
+            InvoiceItemType::Item,
+            InvoiceItemType::Project,
+            InvoiceItemType::DomainAddonIDP,
+            InvoiceItemType::EMPTY,
+            InvoiceItemType::RCOrder,
+            InvoiceItemType::PromoDomain,
+            InvoiceItemType::DomainRedemptionFee => null,
+        };
+
+        if (is_null($rel_class)) {
+            throw new RuntimeException("Relation class not defined for invoice item type: {$this->type->value}");
+        }
+
+        return $rel_class::find($this->relid);
     }
 }
