ac9148ef29eeddc4ca71e7db99447494b6a167a6
aleksandar.naydenov <aleksandar.naydenov@delta.bg>
2026-01-05 14:44:47 +0200

    Add additional properties to `Invoice` model for enhanced data representation

--- src/Models/Invoice.php
+++ src/Models/Invoice.php
@@ -8,6 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * @property int $id
+ * @property int $userid
+ * @property string $invoicenum
+ * @property $date
+ * @property $duedate
+ * @property $datepaid
+ * @property $last_capture_attempt
+ * @property $date_refunded
+ * @property $date_cancelled
+ * @property float $subtotal
+ * @property float $credit
+ * @property float $tax
+ * @property float $tax2
+ * @property float $total
+ * @property float $taxrate
+ * @property float $taxrate2
+ * @property string $status
+ * @property string $paymentmethod
+ * @property int $paymethodid
+ * @property string $notes
  * @property $created_at
  * @property $updated_at
  */
